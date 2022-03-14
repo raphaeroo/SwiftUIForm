a2c2a8573fc1b589b94d4db1c28559d2b0925d31
Raphael Freitas da Silva <o.raphaelfreitas@gmail.com>
2022-03-13 23:07:44 -0300

    Initial Commit

---
+++ formSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  formSwiftUI
+//
+//  Created by Raphael Freitas da Silva on 13/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ formSwiftUI/formSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  formSwiftUIApp.swift
+//  formSwiftUI
+//
+//  Created by Raphael Freitas da Silva on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct formSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
